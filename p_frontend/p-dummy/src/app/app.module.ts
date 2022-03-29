bf0b1674650ee682828eb21de874bd46b67a9e05
ncsp86 <ncsp86@gmail.com>
2022-03-29 22:26:20 +0200

    p_dummy generate components

--- p_frontend/p-dummy/src/app/app.module.ts
+++ p_frontend/p-dummy/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ProfileComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
